469f3c2208a3ea82163cf0a4b59d299b4b267275
Earl Platt III <eaplatt3@gmail.com>
2018-12-27 08:01:23 -0500

    Inital Commit

---
+++ BudgetProgram/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+//Earl A Platt III
+//12/27/18
+//Program to Caculate a Budget
+//
+//User Inputs:
+//-Pay
+//-Bills
+//
+//Program Outputs:
+//-Pay
+//-Bills
+//-Left Over
+//  -Divided In To Saving & Spending
+////////////////////////////////////
+
+using namespace std;
+
+int main() {
+
+
+
+
+	system("pause");
+
+	return 0;
+}
+
