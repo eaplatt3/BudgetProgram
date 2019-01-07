cc953969f16570961ecdc4b282ecf169b2ba5510
Earl Platt III <eaplatt3@gmail.com>
2019-01-07 15:17:59 -0500

    created calc class

---
+++ BudgetProgram/budgetCalc.cpp
@@ -0,0 +1,21 @@
+
+
+class caculation {
+
+	double enteredCost;
+	double enteredPay;
+	
+	caculation() {
+
+	}
+
+	double calc(double pay, double cost) {
+
+		double leftover;
+
+		leftover = cost - pay;
+
+		return leftover;
+	}
+
+};
